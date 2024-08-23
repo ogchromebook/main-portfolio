81b067c89d37c0f3fde7e2a6b3934000ab0cd034
Sam Kachergius <skachergius@gmail.com>
2024-08-22 22:46:17 -0500

    updated AboutMe.jsx file

---
+++ my-portfolio/src/components/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="path/to/your/photo.jpg" alt="Your Name" />
+      <p>Your bio goes here...</p>
+    </section>
+  );
+}
+
+export default AboutMe;
