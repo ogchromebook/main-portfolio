d4ebec093aaa030b1240357f2608089a16a151f7
Sam Kachergius <skachergius@gmail.com>
2024-08-22 22:50:53 -0500

    updated Resume.jsx file

---
+++ my-portfolio/src/components/Resume.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <section>
+      <h2>Resume</h2>
+      <a href="path/to/your/resume.pdf" download>Download Resume</a>
+      <h3>Proficiencies</h3>
+      <ul>
+        <li>Skill 1</li>
+        <li>Skill 2</li>
+        {/* Add more skills */}
+      </ul>
+    </section>
+  );
+}
+
+export default Resume;
