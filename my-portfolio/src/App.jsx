ee35ebdfbf385f2bd11e62fd1a483c147a253703
Sam Kachergius <skachergius@gmail.com>
2024-08-22 22:45:27 -0500

    updated footer.jsx file

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/header';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
