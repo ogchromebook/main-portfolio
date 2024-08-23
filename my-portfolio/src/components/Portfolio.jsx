435fb0df0972a42f61155b6581dfdd17737ff26b
Sam Kachergius <skachergius@gmail.com>
2024-08-23 01:35:15 -0500

    final final touches

--- my-portfolio/src/components/Portfolio.jsx
+++ my-portfolio/src/components/Portfolio.jsx
@@ -7,9 +7,9 @@ import textEditor from '../assets/text-editor.gif';
 
 function Portfolio() {
   const projects = [
-    { title: 'Note Taker', image: noteTaker, deployedLink: 'https://project1.com', githubLink: 'https://github.com/project1' },
-    { title: 'Kanban Board', image: kanbanBoard, deployedLink: 'https://project2.com', githubLink: 'https://github.com/ogchromebook/kanban-board' },
-    { title: 'CLI Employee Manager', image: myEmpGif, deployedLink: 'https://project3.com', githubLink: 'https://github.com/project3' }, // Using GIF here
+    { title: 'Note Taker', image: noteTaker, deployedLink: 'https://ogchromebook.github.io/note-taker/public/', githubLink: 'https://github.com/ogchromebook/note-taker' },
+    { title: 'Kanban Board', image: kanbanBoard, deployedLink: 'https://ogchromebook.github.io/kanban-board/', githubLink: 'https://github.com/ogchromebook/kanban-board' },
+    { title: 'CLI Employee Manager', image: myEmpGif, githubLink: 'https://github.com/ogchromebook/employee-tracker' }, // Using GIF here
     { title: 'Text Editor', image: textEditor, deployedLink: 'https://text-editor-lrf0.onrender.com', githubLink: 'https://github.com/ogchromebook/text-editor' },
   ];
 
